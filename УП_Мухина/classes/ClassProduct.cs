61c7e06dbc3feabed03536a8f3eb0053251ba43f
Дарья Фасхиева <dashaf712@mail.ru>
2023-03-05 23:12:40 +0300

    Добавьте файлы проекта.

---
+++ УП_Мухина/classes/ClassProduct.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace УП_Мухина
+{
+    public partial class Product
+    {
+        public string Name //вывод наименования
+        {
+            get
+            {
+                return TitleProduct.TitleProduct1;
+            }
+        }
+        public string Desc //вывод описания
+        {
+            get
+            {
+                return ProductDescription;
+            }
+        }
+        public string Manufact
+        {
+            get
+            {
+                return "Производитель: " + Manufacturer.ProductManufacturer;
+            }
+        }
+        public string Price
+        {
+            get
+            {
+                return "Цена: " + ProductCost;
+            }
+        }
+        public string Discount
+        {
+            get
+            {
+                return "Скидка " + ProductDiscountAmount + " %";
+            }
+        }
+    }
+}
---
+++ УП_Мухина/pages/PageAuto.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace УП_Мухина
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAuto.xaml
+    /// </summary>
+    public partial class PageAuto : Page
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        int time = 10;
+
+        public PageAuto()
+        {
+            InitializeComponent();
+            tbPassword.Focus();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += new EventHandler(Timer_Tick);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            time--;
+            tbCode.Text = "Получить новый код можно через " + time + " секунд";
+            if (time < 0)
+            {
+                timer.Stop();
+                tbCode.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void tbAuto_Click(object sender, RoutedEventArgs e)
+        {
+            User user = DataBase.Base.User.FirstOrDefault(z => z.UserLogin == tbLogin.Text && z.UserPassword == tbPassword.Password);
+            if (user != null)
+            {
+                ClassFrame.frameL.Navigate(new pages.ListOfTovar());
+               // MessageBox.Show("Пользователя не существует!", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                /*WindowCaptcha captcha = new WindowCaptcha(k);
+                captcha.ShowDialog();*/
+            }
+        }
+
+        private void tbGuest_Click(object sender, RoutedEventArgs e)
+        {
+            ClassFrame.frameL.Navigate(new pages.ListOfTovar());
+        }
+    }
+}
